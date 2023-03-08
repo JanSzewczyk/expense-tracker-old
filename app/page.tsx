96571286442d9e70495d1c1d70c0e01a68699e83
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-03-08 10:49:44 +0100

    ci(pr-check): add PR check action

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <main className="h-screen flex flex-col justify-center items-center">
       <div className="px-8 text-center">
         <h1 className="typography-heading-2">Expense Tracker</h1>
-        <p className="mt-8 typography-heading-5 text-typography-secondary">Let's</p>
-        <p className="typography-heading-4">Track 'Em ALL</p>
+        <p className="mt-8 typography-heading-5 text-typography-secondary">Let&apos;s</p>
+        <p className="typography-heading-4">Track &apos;Em ALL</p>
         <p className="mt-10 typography-subtitle-1 text-typography-disabled ">App IN PROGRESS ...</p>
       </div>
     </main>
